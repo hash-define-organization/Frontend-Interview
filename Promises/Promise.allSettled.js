43aa0ac616d10d13e1f81c16df11a393838e35d9
Shivam Mani Tripathi <31733809+geekcodershivam@users.noreply.github.com>
2024-05-15 10:26:03 +0530

    learn(day2) : Polyfill of promises functions

---
+++ Promises/Promise.allSettled.js
@@ -0,0 +1,45 @@
+/**
+ * @param {Array<any>} promises - notice that input might contains non-promises
+ * @return {Promise<Array<{status: 'fulfilled', value: any} | {status: 'rejected', reason: any}>>}
+ */
+function allSettled(promiseArr) {
+  // your code here
+   if (!promiseArr.length) {
+    return Promise.resolve([]);
+  }
+  let result = [];
+  let count = 0;
+  return new Promise(res => {
+    promiseArr.forEach((promise, idx) => {
+      const curr = Promise.resolve(promise);
+      curr.then(val => {
+        result[idx] = {
+          status: "fulfilled",
+          value: val,
+        }
+      }).catch(err => {
+        result[idx] = {
+          status: "rejected",
+          reason: err,
+        }
+      }).finally(() => {
+        count++;
+        if (count === promiseArr.length) {
+          res(result);
+        }
+      })
+    })
+  })
+}
+
+const promise1 = Promise.resolve(3);
+const promise2 = new Promise((resolve, reject) =>
+  setTimeout(reject, 100, 'foo'),
+);
+const p0 = [];
+const p1 = "shivam";
+const promises = [promise1, promise2, p0, p1];
+
+allSettled(promises)
+  .then((results) => console.log(results))
+
---
+++ Promises/asyncSeriesExecuter.js
@@ -0,0 +1,85 @@
+
+ /**
+ * @param {Array<any>} promises - notice input might have non-Promises
+ * @return {Promise<any[]>}
+ * @Question Execute async functions in Series
+ * @ProblemStatement Implement a function that takes a list of async functions as input and executes them in a series that is one at a time.} ,The next task is executed only when the previous task is completed.
+ */
+
+
+ const asyncTask = function(i) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`Completing ${i}`), 100 * i)
+  });
+}
+
+const promises = [
+  asyncTask(3),
+  asyncTask(1),
+  asyncTask(7),
+  asyncTask(2),
+  asyncTask(5),
+];
+
+// Approach 1 - Using async/await
+const asyncSeriesExecuter = async function(promises) {
+  for (let promise of promises) {
+    try{
+      const result = await promise;
+      console.log(result);
+    }catch(e){
+      console.log(e);
+} }
+}
+
+// Approach 2 - Using recursion + Array.shift()
+const asyncSeriesExecuter = function(promises) {
+  let promise = promises.shift();
+  promise.then((data) => {
+    console.log(data);
+    if (promises.length > 0) {
+      asyncSeriesExecuter(promises);
+}
+ });
+}
+
+// Approach 3 - Using recursion + rest operator
+const asyncSeriesExecuter = function(promises) {
+  const [promise, ...rest] = promises
+  promise.then((data) => {
+    console.log(data);
+    if (rest.length > 0) {
+      asyncSeriesExecuter(rest);
+}
+ });
+}
+
+// Approach 4 - Using Array.reduce()
+function asyncSeriesExecuter(arr) {
+  arr.reduce((acc, curr) => {
+    return acc.then(() => {
+
+      return curr.then(val => console.log(val)); // 
+    })
+  }, Promise.resolve())
+}
+
+
+// Approach 5 - Using recursion
+function asyncSeriesExecuter(arr) {
+  const helper=(arr, idx) => {
+    if(idx === arr.length) return;
+    arr[idx].then((val)=>{
+      console.log(val);
+      helper(arr, idx + 1);
+    })
+
+  }
+
+  helper(arr, 0);
+}
+
+asyncSeriesExecuter(promises);
+
+
+
