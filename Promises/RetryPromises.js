1c281379a7e4e241684000bc5f9d2614d504663f
Shivam Mani Tripathi <31733809+geekcodershivam@users.noreply.github.com>
2024-05-16 12:19:10 +0530

    learn(day3) : Polyfill of promises functions

---
+++ Promises/RetryPromises.js
@@ -0,0 +1,78 @@
+
+ /**
+ * @param {Array<any>} promises - notice input might have non-Promises
+ * @return {Promise<any[]>}
+ * @Question  Retry promises N number of times
+ * @ProblemStatement Implement a function in JavaScript that retries promises N number of times with a delay between each call.
+ **/
+
+// Input:
+// retry(asyncFn, retries = 3, delay = 50, finalError = 'Failed');
+
+// Output:
+// ... attempt 1 -> failed
+// ... attempt 2 -> retry after 50ms -> failed
+// ... attempt 3 -> retry after 50ms -> failed
+// ... Failed.
+
+
+// wait function
+
+const wait = (ms) => new Promise((resolve) =>setTimeout(()=> resolve(), ms));
+const getCount = () => {
+  let c = 0; 1
+  return async ()=> {
+    c++;
+    if(c < 2){
+      throw new Error("Failed to get");
+    }
+  }
+}
+
+// async function retry(asyncFn, retries = 3, delay = 50, finalError = 'Failed'){
+//   try{ 
+//     await asyncFn();
+//   }
+//   catch(err){
+//     console.log(retries, "F")
+//     if(retries <=0){
+//       return Promise.reject(finalError);
+//     }
+//     // wait for delay time 
+//     await wait(delay);
+//     return retry(asyncFn, retries - 1, delay, finalError);
+//   }
+// }
+
+
+// const test= async () =>{
+//   await retry(getCount(), 3)
+//   console.log("success")
+// }
+
+// test().catch(error => console.log("error", error));
+
+
+function retry(asyncFn, retries = 3, delay = 50, finalError = 'Failed'){
+  return new Promise((resolve, reject) =>{
+    return asyncFn().then(resolve).catch(err => {
+      console.log("shivam", retries);
+      if(retries > 0){
+       return wait(delay).then(retry(asyncFn, retries - 1, delay, finalError)).then(resolve).catch(reject);
+      }else{
+       return reject(finalError);
+      }
+
+    })
+  })
+}
+
+const test = async () =>{
+  await retry(getCount(), 3)
+  console.log("success")
+}
+
+test().catch(error => console.log("error"));
+
+
+
---
+++ Promises/asyncParallelExecuter.js
@@ -0,0 +1,38 @@
+
+ /**
+ * @param {Array<any>} promises - notice input might have non-Promises
+ * @return {Promise<any[]>}
+ * @Question Execute async functions in parallel
+ * @ProblemStatement Implement a function that takes a list of async functions as input and a callback function and 
+ * executes the input tasks in parallel i.e all at once and invokes the callback after every task is finished.
+ *
+ **/
+ 
+ function asyncTask(){
+  const val= Math.floor(Math.random() * 10);
+  return function (callback){
+    setTimeout(()=> {
+      callback(val);
+    }, val)
+  }
+ }
+
+ let list = [asyncTask(),asyncTask(),asyncTask(),asyncTask(),asyncTask()]
+
+ function executeParallel(lists , callback){
+  let results = [];
+  let count =0;
+  lists.forEach((fn, idx)=> {
+    fn(val=> {
+       count++;
+       results[idx]= val;
+       if(count=== lists.length){
+        callback(results)
+       }
+    })
+  })
+  
+
+ }
+
+ executeParallel(list, (val)=> {console.log(val)})
